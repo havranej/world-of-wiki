2797c3940077c933946c2e16a5821537ab2082a0
Jan Havránek <j.havranek@gmail.com>
2019-02-19 15:31:47 +0100

    Basic functions

---
+++ functions.R
@@ -0,0 +1,107 @@
+
+library("rjson")
+library("ggplot2")
+library("dplyr")
+library("viridis")
+
+theme.map <- function(...) {
+  theme_minimal() +
+    theme(
+      axis.line = element_blank(),
+      axis.text.x = element_blank(),
+      axis.text.y = element_blank(),
+      axis.ticks = element_blank(),
+      axis.title.x = element_blank(),
+      axis.title.y = element_blank(),
+      panel.grid.major = element_line(color = "#ebebe5", size = 0.2),
+      panel.grid.minor = element_blank(),
+      plot.background = element_rect(fill = "#f5f5f2", color = NA), 
+      panel.background = element_rect(fill = "#f5f5f2", color = NA), 
+      legend.background = element_rect(fill = "#f5f5f2", color = NA),
+      panel.border = element_blank(),
+      ...
+    )
+}
+
+get.links.here.number <- function(page.name){
+  # For normal users limited to 500 :(
+  url <- sprintf("https://en.wikipedia.org/w/api.php?action=query&titles=%s&prop=linkshere&format=json&redirects=TRUE&lhlimit=max", page.name)
+  json.data <- fromJSON(file = url)
+  links.number <- length(json.data$query$pages[[1]]$linkshere)
+  return(links.number)
+}
+
+add.native.language <- function(table, json.data){
+  name <- json.data$parse$title
+  url <- sprintf("https://en.wikipedia.org/wiki/%s", name)
+  
+  native.row <- data.frame(title = name, lang.code = "en", language = "English", url = url, row.names = "en")
+  result <- rbind(table, native.row)
+  return(result)
+}
+
+get.language.variations <- function(page.name){
+  url <- sprintf("https://en.wikipedia.org/w/api.php?action=parse&page=%s&prop=langlinks&format=json&redirects=TRUE", page.name)
+  json.data <- fromJSON(file = url)
+  
+  titles <- sapply(json.data$parse$langlinks, function(x)x$`*`)
+  lang.codes <- sapply(json.data$parse$langlinks, function(x)x$lang)
+  languages <- sapply(json.data$parse$langlinks, function(x)x$langname)
+  urls <- sapply(json.data$parse$langlinks, function(x)x$url)
+  
+  result <- data.frame(title = titles, lang.code = lang.codes, language = languages, url = urls, row.names = lang.codes)
+  result <- add.native.language(result, json.data)
+  return(result)
+}
+
+get.page.content <- function(lang.code, page.name){
+  url <- sprintf("https://%s.wikipedia.org/w/api.php?action=parse&page=%s&prop=text&format=json&redirects=TRUE", lang.code, page.name)
+  json.data <- fromJSON(file = url)
+  result <- json.data$parse$text$`*`
+  return(result)
+}
+
+remove.html <- function(text){
+  return(gsub("<.*?>", "", text))
+}
+
+get.page.length <- function(lang.code, page.name){
+  raw.html <- get.page.content(lang.code, page.name)
+  text <- remove.html(raw.html)
+  len <- nchar(text)
+  return(len)
+}
+
+filter.and.add.states <- function(df){
+  selection <- as.character(LANG.TO.STATE$lang.code)
+  df <- df[selection,]
+  df <- cbind(df, region = LANG.TO.STATE$region) ### ! troublesome, if the article in the given language doesn't exist
+  return(df)
+}
+
+add.values <- function(df){
+  values <- apply(df, 1, function(row) get.page.length(row["lang.code"], row["title"]))
+  result <- cbind(df, value = values)
+  return(result)
+}
+
+add.coordinates <- function(df){
+  result <- merge(MAP.DATA, df, by = "region")
+  result <- result[order(result$order),]
+  return(result)
+}
+
+MAP.DATA <- map_data("world")
+LANG.TO.STATE <- data.frame(lang.code = c("cs", "de", "de", "pl", "sk", "fr", "nl"), 
+                            region = c("Czech Republic", "Germany", "Austria", "Poland", "Slovakia", "France", "Netherlands"))
+
+
+page.name <- "Prague"
+x <- get.language.variations(page.name) %>% filter.and.add.states() %>% add.values() %>% add.coordinates()
+
+ggplot(x, aes(long, lat, group=group, fill=value)) + geom_polygon(color="grey") + #theme.map() +
+  scale_fill_viridis(option = "viridis", direction = -1) + 
+  labs(x = NULL, 
+       y = NULL, 
+       title = page.name, 
+       subtitle = "Wikipedia page size in major language of the state")
